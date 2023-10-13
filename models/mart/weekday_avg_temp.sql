b221843814f6821bbccbd1896858e0a6cc5b12f0
jannuccia <janna.rheinbay@gmail.com>
2023-10-13 19:15:31 +0200

    up

--- models/mart/weekday_avg_temp.sql
+++ models/mart/weekday_avg_temp.sql
@@ -1,12 +1,12 @@
-with weekday_avg as
-select
-    -- base_currency,
-    -- avg(gbp) as avg_gbp
-    -- avg(usd) as avg_usd
-    -- avg(inr) as avg_inr
-from {{ref('prep_temp')}}
--- group by base_currency
-)
+-- with weekday_avg as
+-- select
+--     -- base_currency,
+--     -- avg(gbp) as avg_gbp
+--     -- avg(usd) as avg_usd
+--     -- avg(inr) as avg_inr
+-- from {{ref('prep_temp')}}
+-- -- group by base_currency
+-- )
 
-select *
-from weekday_avg
+-- select *
+-- from weekday_avg
